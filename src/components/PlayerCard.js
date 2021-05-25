951894522303959705d0df92c03c7fafd4c2347b
Admin <admin@Admins-MacBook-Air.local>
2021-05-25 08:11:07 +0100

    Player image moved to Public folder

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Thiago_Silva_2 from '/Users/admin/benswebsite/src/Thiago_Silva_2.jpg'
 import '../App.css'
 
+
 const PlayerCard = () => {
-   return ( <img src={Thiago_Silva_2} alt="Thiago Silva" className='PlayerCard1'>
+   return ( <img src={process.env.PUBLIC_URL + '/Users/admin/benswebsite/public/Thiago_Silva_2.jpg'} alt="Thiago Silva" className='PlayerCard1'>
 
    </img> );
 }

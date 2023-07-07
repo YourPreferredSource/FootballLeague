21ede24df505a648fb96fd056bf0e12f6c4c87c0
unknown <gethin.rawlings@eori.uk>
2023-07-07 19:18:26 +0100

    Update to footy package

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import Table from './Table'
-import '../App.css'
+import React, { useEffect, useState, useContext } from 'react';
+// import Table from './Table'
+// import '../App.css'
 import './loginPage.css'
 import { NavLink } from 'react-router-dom'
 
 
 
 const LoginPage = () => {
+
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (event) =>{
+        event.preventDefault();
+        console.log('here yar ', event)
+        console.log('here yar ', username)
+        console.log('here yar ', password)
+    }
+
     return (
 
 
@@ -15,17 +26,30 @@ const LoginPage = () => {
         
        <div className = 'LoginGrid'>
            <div className = 'leftPanel'>
-                <form className = 'loginForm'>
-                    <label LabelText = {'Username/Email Address'}> 
-                    </label>
-                    <input className = 'inputLeft' type = {'email'} placeholder = {'Username'}>
-                    </input>
-                    <label >
-                    </label>
-                    <input className = 'inputLeft' type = {'password'}placeholder = {'Password'}>
-                    </input>
-                    
-                        <button type = 'button' className = 'loginLeftButton'> Login </button>
+                <form className = 'loginForm' onSubmit={handleSubmit}>
+                    {/* <label LabelText = {'Username/Email Address'}> 
+                    </label> */}
+                    <input 
+                    className = 'inputLeft' 
+                    type = 'email' 
+                    placeholder = 'Username' 
+                    value = {username} 
+                    onChange={e => setUserName(e.target.value)}
+                    />
+
+                    {/* <label >
+                    </label> */}
+                    <input 
+                    className = 'inputLeft' 
+                    type = 'password'
+                    placeholder = 'Password'
+                    value = {password} 
+                    onChange={e => setPassword(e.target.value)}
+                    />
+                        
+
+                        <input type="submit" value = 'Login' className = 'loginLeftButton'/>
+                        {/* <button type = 'button' className = 'loginLeftButton' > Login </button> */}
                     
                     
                         <NavLink to = '/CreateAccount' type = 'button' className = 'loginLeftButton'> Create Account </NavLink>
@@ -34,9 +58,9 @@ const LoginPage = () => {
                 
            </div>
            <div className = 'centerPanel'>    
-           <Table>
+           {/* <Table>
                     
-                </Table>         
+                </Table>          */}
            </div>
            <div className = 'rightPanel'>
                <form className = 'loginForm'>
